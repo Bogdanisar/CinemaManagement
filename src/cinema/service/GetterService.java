a6868dab86fd6b2a34c2f5c873a66e3f5f0c5a33
Bogdan <burcea.bogdan.madalin@gmail.com>
2019-04-29 22:54:25 +0300

    Added SetterService.
    - Need to add Associative_Entry class and corresponding Converter, GetterService and SetterService Code.

--- src/cinema/service/GetterService.java
+++ src/cinema/service/GetterService.java
@@ -5,144 +5,139 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GetterService {
-    public static Identifiable getIdentifiable(String pathName, long targetId, Converter converter) {
+    public static Identifiable getIdentifiable(String pathName, long targetId, Converter converter) throws IOException {
         FileReader reader = null;
         CSVParser parser = null;
         Identifiable ans = null;
 
-        try {
-            reader = new FileReader(pathName);
-            parser = new CSVParser(reader, CSVFormat.DEFAULT);
+        reader = new FileReader(pathName);
+        parser = new CSVParser(reader, CSVFormat.DEFAULT);
 
-            for (CSVRecord record : parser) {
-                Identifiable currentIdentifiable = converter.convert(record);
-                if (targetId == currentIdentifiable.getId()) {
-                    return currentIdentifiable;
-                }
+        for (CSVRecord record : parser) {
+            Identifiable currentIdentifiable = converter.convert(record);
+            if (targetId == currentIdentifiable.getId()) {
+                return currentIdentifiable;
             }
         }
-        catch (Exception except) {
-            except.printStackTrace();
-            System.out.println();
-        }
+        parser.close();
 
         return ans;
     }
 
-    public static Auditorium getAuditorium(long id) {
+    public static Auditorium getAuditorium(long id) throws IOException {
         return (Auditorium) GetterService.getIdentifiable(DatabaseConstants.AUDITORIUM_FILE, id, new Converter.Auditorium());
     }
 
-    public static Screening getScreening(long id) {
+    public static Screening getScreening(long id) throws IOException {
         return (Screening) GetterService.getIdentifiable(DatabaseConstants.SCREENING_FILE ,id, new Converter.Screening());
     }
 
-    public static Category getCategory(long id) {
+    public static Category getCategory(long id) throws IOException {
         return (Category) GetterService.getIdentifiable(DatabaseConstants.CATEGORY_FILE ,id, new Converter.Category());
     }
 
-    public static Movie getMovie(long id) {
+    public static Movie getMovie(long id) throws IOException {
         return (Movie) GetterService.getIdentifiable(DatabaseConstants.MOVIE_FILE ,id, new Converter.Movie());
     }
 
-    public static Food getFood(long id) {
+    public static Food getFood(long id) throws IOException {
         return (Food) GetterService.getIdentifiable(DatabaseConstants.FOOD_FILE ,id, new Converter.Food());
     }
 
-    public static Client getClient(long id) {
+    public static Client getClient(long id) throws IOException {
         return (Client) GetterService.getIdentifiable(DatabaseConstants.CLIENT_FILE ,id, new Converter.Client());
     }
 
-    public static Employee getEmployee(long id) {
+    public static Employee getEmployee(long id) throws IOException {
         return (Employee) GetterService.getIdentifiable(DatabaseConstants.EMPLOYEE_FILE ,id, new Converter.Employee());
     }
 
-    public static TicketPurchase getTicketPurchase(long id) {
+    public static TicketPurchase getTicketPurchase(long id) throws IOException {
         return (TicketPurchase) GetterService.getIdentifiable(DatabaseConstants.TICKET_PURCHASE_FILE ,id, new Converter.TicketPurchase());
     }
 
-    public static FoodPurchase getFoodPurchase(long id) {
+    public static FoodPurchase getFoodPurchase(long id) throws IOException {
         return (FoodPurchase) GetterService.getIdentifiable(DatabaseConstants.FOOD_PURCHASE_FILE ,id, new Converter.FoodPurchase());
     }
 
 
 
-    public static List<Identifiable> getAll(String pathName, Converter converter) {
+
+
+    public static List<Identifiable> getAll(String pathName, Converter converter) throws IOException {
         FileReader reader = null;
         CSVParser parser = null;
 
-        List<Identifiable> ret = new ArrayList<>();
-        try {
-            reader = new FileReader(pathName);
-            parser = new CSVParser(reader, CSVFormat.DEFAULT);
+        reader = new FileReader(pathName);
+        parser = new CSVParser(reader, CSVFormat.DEFAULT);
 
-            for (CSVRecord record : parser) {
-                Identifiable currentIdentifiable = converter.convert(record);
-                ret.add(currentIdentifiable);
-            }
+        List<Identifiable> ret = new ArrayList<>();
+        for (CSVRecord record : parser) {
+            Identifiable currentIdentifiable = converter.convert(record);
+            ret.add(currentIdentifiable);
         }
-        catch (Exception except) {
-            except.printStackTrace();
-            System.out.println();
-        }
+
+        parser.close();
 
         return ret;
     }
 
-    public static List<cinema.data.Auditorium> getAllAuditorium() {
+    public static List<cinema.data.Auditorium> getAllAuditorium() throws IOException {
         return Converter.cast(
                 GetterService.getAll(DatabaseConstants.AUDITORIUM_FILE, new Converter.Auditorium())
         );
     }
 
-    public static List<cinema.data.Category> getAllCategory() {
+    public static List<cinema.data.Category> getAllCategory() throws IOException {
         return Converter.cast(
                 GetterService.getAll(DatabaseConstants.CATEGORY_FILE, new Converter.Category())
         );
     }
 
-    public static List<cinema.data.Client> getAllClient() {
+    public static List<cinema.data.Client> getAllClient() throws IOException {
         return Converter.cast(
                 GetterService.getAll(DatabaseConstants.CLIENT_FILE, new Converter.Client())
         );
     }
 
-    public static List<cinema.data.Employee> getAllEmployee() {
+    public static List<cinema.data.Employee> getAllEmployee() throws IOException {
         return Converter.cast(
                 GetterService.getAll(DatabaseConstants.EMPLOYEE_FILE, new Converter.Employee())
         );
     }
 
-    public static List<cinema.data.Food> getAllFood() {
+    public static List<cinema.data.Food> getAllFood() throws IOException {
         return Converter.cast(
                 GetterService.getAll(DatabaseConstants.FOOD_FILE, new Converter.Food())
         );
     }
 
-    public static List<cinema.data.FoodPurchase> getAllFoodPurchase() {
+    public static List<cinema.data.FoodPurchase> getAllFoodPurchase() throws IOException {
         return Converter.cast(
                 GetterService.getAll(DatabaseConstants.FOOD_PURCHASE_FILE, new Converter.FoodPurchase())
         );
     }
 
-    public static List<cinema.data.Movie> getAllMovie() {
+    public static List<cinema.data.Movie> getAllMovie() throws IOException {
         return Converter.cast(
                 GetterService.getAll(DatabaseConstants.MOVIE_FILE, new Converter.Movie())
         );
     }
 
-    public static List<cinema.data.Screening> getAllScreening() {
+    public static List<cinema.data.Screening> getAllScreening() throws IOException {
         return Converter.cast(
                 GetterService.getAll(DatabaseConstants.SCREENING_FILE, new Converter.Screening())
         );
     }
 
-    public static List<cinema.data.TicketPurchase> getAllTicketPurchase() {
+    public static List<cinema.data.TicketPurchase> getAllTicketPurchase() throws IOException {
         return Converter.cast(
                 GetterService.getAll(DatabaseConstants.TICKET_PURCHASE_FILE, new Converter.TicketPurchase())
         );
--- src/cinema/service/SetterService.java
+++ src/cinema/service/SetterService.java
@@ -1,5 +1,87 @@
 package cinema.service;
 
+import cinema.data.Identifiable;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SetterService {
-    
+    public static void update(String pathName, Converter converter, Identifiable updateObject) throws IOException {
+        FileReader reader = new FileReader(pathName);
+        CSVParser parser = new CSVParser(reader, CSVFormat.DEFAULT);
+
+        List<Identifiable> list = new ArrayList<>();
+        boolean matchFound = false;
+        for (CSVRecord record : parser) {
+            Identifiable currentIdentifiable = converter.convert(record);
+
+            Identifiable nextIdentifiable = currentIdentifiable;
+            if (currentIdentifiable.getId() == updateObject.getId()) {
+                if (matchFound) {
+                    throw new IllegalArgumentException("Two objects with the same id(" + updateObject.getId() + ") found in " + pathName);
+                }
+                else {
+                    matchFound = true;
+                    nextIdentifiable = updateObject;
+                }
+            }
+
+            list.add(nextIdentifiable);
+        }
+        parser.close();
+
+        if (matchFound == false) {
+            list.add(updateObject);
+        }
+
+        CSVPrinter printer = new CSVPrinter(new FileWriter(pathName), CSVFormat.DEFAULT);
+        for (Identifiable currIdentifiable : list) {
+            converter.parse(printer, currIdentifiable);
+        }
+        printer.close();
+    }
+
+    public static void update(cinema.data.Auditorium object) throws IOException {
+        SetterService.update(DatabaseConstants.AUDITORIUM_FILE, new Converter.Auditorium(), object);
+    }
+
+    public static void update(cinema.data.Category object) throws IOException {
+        SetterService.update(DatabaseConstants.CATEGORY_FILE, new Converter.Category(), object);
+    }
+
+    public static void update(cinema.data.Client object) throws IOException {
+        SetterService.update(DatabaseConstants.CLIENT_FILE, new Converter.Client(), object);
+    }
+
+    public static void update(cinema.data.Employee object) throws IOException {
+        SetterService.update(DatabaseConstants.EMPLOYEE_FILE, new Converter.Employee(), object);
+    }
+
+    public static void update(cinema.data.Food object) throws IOException {
+        SetterService.update(DatabaseConstants.FOOD_FILE, new Converter.Food(), object);
+    }
+
+    public static void update(cinema.data.FoodPurchase object) throws IOException {
+        SetterService.update(DatabaseConstants.FOOD_PURCHASE_FILE, new Converter.FoodPurchase(), object);
+    }
+
+    public static void update(cinema.data.Movie object) throws IOException {
+        SetterService.update(DatabaseConstants.MOVIE_FILE, new Converter.Movie(), object);
+    }
+
+    public static void update(cinema.data.Screening object) throws IOException {
+        SetterService.update(DatabaseConstants.SCREENING_FILE, new Converter.Screening(), object);
+    }
+
+    public static void update(cinema.data.TicketPurchase object) throws IOException {
+        SetterService.update(DatabaseConstants.TICKET_PURCHASE_FILE, new Converter.TicketPurchase(), object);
+    }
 }
